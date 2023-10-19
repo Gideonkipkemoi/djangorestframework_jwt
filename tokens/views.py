e8bb9ae099a806c7b92ef1170a67555fbb19d300
GideonKipkemoi <rotichgideon760@gmail.com>
2023-10-19 08:15:20 +0300

    required imports made

--- tokens/views.py
+++ tokens/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
